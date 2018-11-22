96bb1a44c07167628a36348b965870037edf0274
Kurt Nelson <kurtn@uber.com>
2018-11-21 16:11:38 -0800

    Add non-null annotations

--- java/com/ubercab/simplestorage/SimpleStore.java
+++ java/com/ubercab/simplestorage/SimpleStore.java
@@ -1,20 +1,22 @@
 package com.ubercab.simplestorage;
 
+import java.io.Closeable;
 import java.util.concurrent.Executor;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 
-public interface SimpleStore {
+public interface SimpleStore extends Closeable {
 
     SimpleStore scope(String name);
     SimpleStore scope(String name, ScopeConfig config);
 
-    void getString(String key, Callback<String> cb, Executor executor);
-    void putString(String key, String value, Callback<String> cb, Executor executor);
+    void getString(String key, @Nonnull Callback<String> cb, @Nonnull Executor executor);
+    void putString(String key, @Nullable String value, @Nonnull Callback<String> cb, @Nonnull Executor executor);
 
-    void get(String key, Callback<byte[]> cb, Executor executor);
-    void put(String key, @Nullable byte[] value, Callback<byte[]> cb, Executor executor);
+    void get(String key, @Nonnull Callback<byte[]> cb, @Nonnull Executor executor);
+    void put(String key, @Nullable byte[] value, @Nonnull Callback<byte[]> cb, @Nonnull Executor executor);
 
     void deleteAll(Callback<Void> cb, Executor executor);
 
