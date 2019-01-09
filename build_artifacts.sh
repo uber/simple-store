a9389a8785bd48e2eb23613b59c46b8b6c2f6d8f
Kurt Nelson <kurtn@uber.com>
2019-01-09 15:20:45 -0800

    Gradle-fy

--- build_artifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bazel build //java/com/uber/simplestore/proto:bin_deploy.jar
-cp bazel-bin/java/com/uber/simplestore/proto/bin_deploy.jar simplestore-proto.jar
-chmod u+rw simplestore-proto.jar
-zip -d simplestore-proto.jar "com/google/*"
-zip -d simplestore-proto.jar "javax/*"
-zip -d simplestore-proto.jar "META-INF/maven/*"
